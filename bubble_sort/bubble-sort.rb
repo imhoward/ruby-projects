0017c4a6bddad9ced572bffab6061cca40ae4589
Ian Howard <howardian58@gmail.com>
2022-08-07 12:42:07 -0400

    Add comments for big O notation

--- bubble_sort/bubble-sort.rb
+++ bubble_sort/bubble-sort.rb
@@ -1,11 +1,15 @@
 array_to_sort = [50, 29, 15, 12, 1]
 
+# Worst case = O(n^2), best case = O(n)
 def bubble_sort(array)
     sorted = false
     new_array = array
 
+    # Worst case = O(n), best case = O(1)
     while !sorted
         switches = 0
+
+        # O(n-1) -> Iterates through all elements except the last
         new_array.each_with_index {
             |num, i|
             
@@ -14,7 +18,6 @@ def bubble_sort(array)
             end
 
             if num > new_array[i+1]
-                #temp_val = num
                 new_array[i] = new_array[i+1]
                 new_array[i+1] = num
                 switches += 1
